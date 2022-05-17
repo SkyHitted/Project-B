dbcd262c9699189fcd1df3848453eb28e255d7ab
SkyHitted <95493454+SkyHitted@users.noreply.github.com>
2022-05-17 10:29:08 +0200

    projectBGroep3

---
+++ projectBGroep3/projectBGroep3/ReserverenClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace projectBGroep3
+{
+    class ReserverenClass
+    {
+        public string deDatum { get; set; }
+        public List<slotsAndReseveren> allTime { get; set; }
+    }
+    class slotsAndReseveren
+    {
+        public Dictionary<int, int> openSlotten { get; set; }
+        public Dictionary<int, int> reserveert { get; set; }
+    }
+    class NewDay
+    {
+        public List<ReserverenClass> Reserveren { get; set; }
+    }
+}
