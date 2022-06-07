fa001d2bac3d962b0ce48e3614a885da4fcb691f
SkyHitted <95493454+SkyHitted@users.noreply.github.com>
2022-06-07 10:05:00 +0200

    Add files via upload

---
+++ projectBGroep3/projectBGroep3/ReserverenJson.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace projectBGroep3
+{
+    class ReserverenJson
+    {
+        public string deDatum { get; set; }
+        public List<slotsAndReseveren> allTime { get; set; }
+    }
+    class slotsAndReseveren
+    {
+        public Dictionary<int, int> openSlotten { get; set; }
+        public Dictionary<int, int> reserveert { get; set; }
+    }
+    class NewDay
+    {
+        public List<ReserverenJson> Reserveren { get; set; }
+    }
+}
