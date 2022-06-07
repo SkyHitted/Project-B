02b452f070c04b08e0315b940957fc47238c94ef
SkyHitted <95493454+SkyHitted@users.noreply.github.com>
2022-06-07 10:19:56 +0200

    Add files via upload

---
+++ projectBGroep3/projectBGroep3/AccountJson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+// json stuff
+namespace projectBGroep3
+{
+    class AccountJson
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string typeOfAccount { get; set; }
+    }
+    class TypesOfChoice
+    {
+        public List<AccountJson> Accounts { get; set; }
+    }
+}
---
+++ projectBGroep3/projectBGroep3/Options.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+// class voor opties
+namespace projectBGroep3
+{
+    class Options
+        {
+            public static int listOfChoice(bool canCancel, string saySomething, params string[] options)
+            {
+                // const int startY = 8;
+
+                int currentSelection = 0;
+
+                ConsoleKey key;
+
+                Console.CursorVisible = false;
+
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine(saySomething);
+                /*
+                if (options.Contains("Ga verder als gast\n"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Navigeer de applicatie doormiddel van arrow keys");
+                    Console.ResetColor();
+                }*/
+
+                    for (int i = 0; i < options.Length; i++)
+                    {
+                        // Console.SetCursorPosition(0, startY + i);
+
+                        if (i == currentSelection)
+                            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+                        Console.Write(options[i]);
+
+                        Console.ResetColor();
+                    }
+                    key = Console.ReadKey(true).Key;
+
+                    switch (key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            {
+                                if (currentSelection > 0)
+                                    currentSelection--;
+                                break;
+                            }
+                        case ConsoleKey.DownArrow:
+                            {
+                                if (currentSelection < options.Length - 1)
+                                    currentSelection++;
+                                break;
+                            }
+                        case ConsoleKey.Escape:
+                            {
+                                if (canCancel)
+                                    return -1;
+                                break;
+                            }
+                    }
+                } while (key != ConsoleKey.Enter);
+
+                Console.CursorVisible = true;
+
+                return currentSelection;
+            }
+        }
+}
